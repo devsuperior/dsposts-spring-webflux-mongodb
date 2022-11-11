28e96a89fc5f82da8a8ad3580650b536dd6462e9
Alexandre <alexandre.si.ufu@gmail.com>
2022-11-11 12:37:10 -0300

    GET find posts by title

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,15 +1,20 @@
 package com.devsuperior.workshopmongo.controllers;
 
+import java.io.UnsupportedEncodingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.workshopmongo.controllers.util.URL;
 import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.services.PostService;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -22,5 +27,12 @@ public class PostController {
 	@GetMapping(value = "/{id}")
 	public Mono<ResponseEntity<PostDTO>> findById(@PathVariable String id) {
 		return service.findById(id).map(postDto -> ResponseEntity.ok().body(postDto));
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public Flux<PostDTO> findByTitle(@RequestParam(value = "text", defaultValue = "") String text)
+			throws UnsupportedEncodingException {
+		text = URL.decodeParam(text);
+		return service.findByTitle(text);
 	}
 }
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -19,6 +20,11 @@ public class PostService {
 		return repository.findById(id)
 				.map(existingPost -> new PostDTO(existingPost))
 				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
+	}
+	
+	public Flux<PostDTO> findByTitle(String text) {
+		return repository.searchTitle(text)
+				.map(postFound -> new PostDTO(postFound));
 	}
 
 }
